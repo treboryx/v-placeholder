709464d00fedb0cc334464f4b7646d8f5a6f5475
Robert <treboryx@gmail.com>
2022-02-16 22:27:02 +0200

    add readme

--- example/src/main.js
+++ example/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import vPlaceholder from "../../dist/library.mjs";
+// import vPlaceholder from "../../dist/library.mjs";
+import vPlaceholder from "v-placeholder";
 
 createApp(App).use(vPlaceholder).mount("#app");
